0e26a1f03b81840b144c37f5e5ef52530f57dce9
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-08-21 21:33:01 +0530

    Add files via upload

---
+++ udf.py
@@ -0,0 +1,69 @@
+"""
+User Defined Functions(udf):
+
+    A udf is a user defined function.
+"""
+
+# Without Arguments and without return value
+
+# def add():                       # Function definition
+#     num1 = int(input("Enter first number: "))
+#     num2 = int(input("Enter second number: "))
+
+#     add = num1 + num2
+
+#     print("Addition of two numbers is: ", add)
+
+
+# add()                  # Calling the function
+# add()
+# add()
+
+# With Arguments and without return value
+
+# def add(num1, num2):
+#     add = num1 + num2
+
+#     print("Addition of two numbers is: ", add)
+
+# add(12,15)
+# add(int(input("Enter first number: ")), int(input("Enter second number: ")))
+
+"""
+Make a function named calc and take choice(+,-,*,/) from the user and perform the operation.
+"""
+
+# Without Arguments and with return value
+
+# def add():
+#     num1 = int(input("Enter first number: "))
+#     num2 = int(input("Enter second number: "))
+
+#     return num1 + num2
+
+
+# ans = add()
+
+# print("Addition of two numbers is: ", ans)
+
+# With Arguments and with return value
+
+# def add(num1, num2):
+#     return num1 + num2
+
+# print(add(12,15))
+
+"""
+Tasks:
+-------------
+1. Make a function that takes two arguments like starting range and ending range from the user and print all the Armstrong Numbers between them.
+2. Make a function that takes two arguments like starting range and ending range from the user and print all the Palindrome Numbers between them.
+3. Make a function that takes two arguments like starting range and ending range from the user and print all the Twin Numbers between them.
+4. Make a function that takes two arguments like starting range and ending range from the user and print all the Prime Numbers between them.
+5. Make a function that find maximum numbers from the given 3 numbers.
+6. Make a function that took number from the user and check whether it is Armstrong Number or not with return type.
+7. Make a function that took number from the user and check whether it is Prime Number or not with return type.
+8. Make a function that took number from the user and check whether it is Twin Number or not with return type.
+9. Make a function that took number from the user and check whether it is Palindrome Number or not with return type.
+
+"""
